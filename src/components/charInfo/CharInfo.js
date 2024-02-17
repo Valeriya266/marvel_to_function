f78b2124f957aef55380f3ecfb52ea0177ab5f18
valeriya <valeriyastoyan26@gmail.com>
2024-02-17 14:47:53 +0300

    add for comics of hero some add

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -81,11 +81,15 @@ class CharInfo extends Component {
 
 const View = ({char}) => {
     const {name, description, thumbnail, homepage, wiki, comics} = char;
+    let imgStyle = {'objectFit' : 'cover'};
+    if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+        imgStyle = {'objectFit' : 'contain'};
+    }
 
     return(
         <>
             <div className="char__basics">
-                <img src={thumbnail} alt={name}/>
+                <img src={thumbnail} alt={name} style={imgStyle}/>
                     <div>
                         <div className="char__info-name">{name}</div>
                         <div className="char__btns">
@@ -103,8 +107,10 @@ const View = ({char}) => {
             </div>
             <div className="char__comics">Comics:</div>
             <ul className="char__comics-list">
+                {comics.length > 0 ? null : 'There is no comics with this Hero!'}
                 {
                     comics.map((item, i) => {
+                        if (i < 10) 
                         return (
                             <li key={i} className="char__comics-item">
                                 {item.name}
